5793994778879fed38282b9ac8e00034b50fdace
baked-potatoes <70744212+baked-potatoes@users.noreply.github.com>
2026-05-13 18:44:20 -0700

    fix tracking

--- nvim/lua/plugins/vim-fugitive.lua
+++ nvim/lua/plugins/vim-fugitive.lua
@@ -1,12 +1,11 @@
 return {
-  {
-    "tpope/vim-fugitive",
-    cmd = { "Git", "G", "Gdiffsplit", "Gblame" },
-
-    keys = {
-      { "<leader>gs", "<cmd>Git<cr>", desc = "Git status (Fugitive)" },
---      { "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff split" },
---      { "<leader>gb", "<cmd>Gblame<cr>", desc = "Git blame" },
+    {
+        "tpope/vim-fugitive",
+        cmd = { "Git", "G", "Gdiffsplit", "Gblame" },
+        keys = {
+            { "<leader>gs", "<cmd>Git<cr>", desc = "Git status (Fugitive)" },
+            { "<leader>gd", "<cmd>Gdiffsplit<cr>", desc = "Git diff split" },
+            { "<leader>gb", "<cmd>Gblame<cr>", desc = "Git blame" },
+        },
     },
-  },
 }
